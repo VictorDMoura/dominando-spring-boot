cbb1317410ee64a94140df4e38dd0fb868fe3371
vmalmeida <vmalmeidasefaz@gmail.com>
2024-02-19 09:27:45 -0300

    video14: post mapping values

---
+++ src/main/java/academy/devdojo/controller/ProducerController.java
@@ -0,0 +1,23 @@
+package academy.devdojo.controller;
+
+import academy.devdojo.domain.Producer;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@RestController
+@RequestMapping(path = {"v1/producers", "v1/producers/"})
+public class ProducerController {
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE,
+    headers = "X-API-VERSION=v1")
+    public Producer save(@RequestBody Producer producer) {
+        producer.setId(ThreadLocalRandom.current().nextLong(100_000));
+        Producer.list().add(producer);
+        return producer;
+    }
+}
