0a82a0172304deaa811979144d957157c6ade192
vmalmeida <vmalmeidasefaz@gmail.com>
2024-02-22 11:14:27 -0300

    video23: mvc pt1

--- src/main/java/academy/devdojo/domain/Producer.java
+++ src/main/java/academy/devdojo/domain/Producer.java
@@ -4,8 +4,6 @@ package academy.devdojo.domain;
 import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
@@ -18,17 +16,4 @@ public class Producer {
     private String name;
     private LocalDateTime createdAt;
 
-    private static List<Producer> producers = new ArrayList<>();
-
-    static {
-        var mappa = Producer.builder().id(1L).name("Mappa").createdAt(LocalDateTime.now()).build();
-        var kyotoAnimatio = Producer.builder().id(2L).name("Kyoto Animation").createdAt(LocalDateTime.now()).build();
-        var madhouse = Producer.builder().id(3L).name("Madhouse").createdAt(LocalDateTime.now()).build();
-
-        producers.addAll(List.of(mappa, kyotoAnimatio, madhouse));
-    }
-
-    public static List<Producer> list() {
-        return producers;
-    }
 }
---
+++ src/main/java/academy/devdojo/repository/ProducerHardCodedRepository.java
@@ -0,0 +1,48 @@
+package academy.devdojo.repository;
+
+import academy.devdojo.domain.Producer;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ProducerHardCodedRepository {
+
+    private static final List<Producer> PRODUCERS = new ArrayList<>();
+
+    static {
+        var mappa = Producer.builder().id(1L).name("Mappa").createdAt(LocalDateTime.now()).build();
+        var kyotoAnimatio = Producer.builder().id(2L).name("Kyoto Animation").createdAt(LocalDateTime.now()).build();
+        var madhouse = Producer.builder().id(3L).name("Madhouse").createdAt(LocalDateTime.now()).build();
+
+        PRODUCERS.addAll(List.of(mappa, kyotoAnimatio, madhouse));
+    }
+
+    public List<Producer> findAll() {
+        return PRODUCERS;
+    }
+
+    public Optional<Producer> findById(Long id) {
+        return PRODUCERS.stream().filter(p -> p.getId().equals(id)).findFirst();
+    }
+
+    public List<Producer> findByName(String name) {
+        return PRODUCERS.stream().filter(p -> p.getName().equalsIgnoreCase(name)).toList();
+    }
+
+    public Producer save(Producer producer) {
+        PRODUCERS.add(producer);
+        return producer;
+    }
+
+    public void deleteById(Long id) {
+        PRODUCERS.removeIf(p -> p.getId().equals(id));
+    }
+
+    public void update(Producer producer){
+        deleteById(producer.getId());
+        save(producer);
+    }
+
+}
