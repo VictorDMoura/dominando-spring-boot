b7f7d9aed8985e7990ce86283d1941c9f3671992
vmalmeida <vmalmeidasefaz@gmail.com>
2024-02-23 09:59:17 -0300

    video29: @Configuration and @Bean

---
+++ src/main/java/academy/devdojo/config/BeanConfig.java
@@ -0,0 +1,21 @@
+package academy.devdojo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import test.outside.Connection;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+//    @Primary
+    public Connection connectionMySql(){
+        return new Connection("localhost", "mysql", "xxxxx");
+    }
+
+    @Bean(name = "mongoDb")
+    public Connection connectionMongoDb(){
+        return new Connection("localhost", "mongodb", "xxxxx");
+    }
+
+}
--- src/main/java/academy/devdojo/repository/ProducerHardCodedRepository.java
+++ src/main/java/academy/devdojo/repository/ProducerHardCodedRepository.java
@@ -1,7 +1,11 @@
 package academy.devdojo.repository;
 
 import academy.devdojo.domain.Producer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
+import test.outside.Connection;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -9,9 +13,13 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
+@Log4j2
 public class ProducerHardCodedRepository {
 
     private static final List<Producer> PRODUCERS = new ArrayList<>();
+    @Qualifier(value = "mongoDb")
+    private final Connection connection;
 
     static {
         var mappa = Producer.builder().id(1L).name("Mappa").createdAt(LocalDateTime.now()).build();
@@ -30,6 +38,7 @@ public class ProducerHardCodedRepository {
     }
 
     public List<Producer> findByName(String name) {
+        log.info(connection);
         return name == null ? findAll() :
                 PRODUCERS
                         .stream()
