462c726a35392ed6ed18fe74a053f19730650e88
vmalmeida <vmalmeidasefaz@gmail.com>
2024-02-24 21:27:34 -0300

    video34: change findById on service

--- src/main/java/academy/devdojo/service/ProducerService.java
+++ src/main/java/academy/devdojo/service/ProducerService.java
@@ -29,14 +29,14 @@ public class ProducerService {
     }
 
     public void deleteById(Long id) {
-        var producer = producerRepository.findById(id)
+        var producer = findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
                         "Producer not found to be deleted"));
         producerRepository.deleteById(producer.getId());
     }
 
     public void update(Producer producerToUpdate) {
-        var producer = producerRepository.findById(producerToUpdate.getId())
+        var producer = findById(producerToUpdate.getId())
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
                         "Producer not found to be updated"));
         producerToUpdate.setCreatedAt(producer.getCreatedAt());
