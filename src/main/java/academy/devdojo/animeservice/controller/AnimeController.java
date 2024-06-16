55be018703be14bb3889d4bff59b6469332df277
VictorDMoura <mouramvictor@gmail.com>
2024-06-16 12:10:37 -0300

    video11: adicionando lombok

--- src/main/java/academy/devdojo/animeservice/controller/AnimeController.java
+++ src/main/java/academy/devdojo/animeservice/controller/AnimeController.java
@@ -1,17 +1,20 @@
 package academy.devdojo.animeservice.controller;
 
 import academy.devdojo.animeservice.domain.Anime;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(path = {"v1/animes", "v1/animes/"})
+@Log4j2
 public class AnimeController {
 
     @GetMapping
     public List<Anime> list(@RequestParam(required = false) String name) {
         var animes = Anime.getAnimes();
+        log.info("Request received to list all animes, param name '{}'", name);
         if (name == null) return animes;
         return animes.stream()
                 .filter(anime -> anime.getName().equalsIgnoreCase(name))
@@ -20,6 +23,7 @@ public class AnimeController {
 
     @GetMapping("{id}")
     public Anime findById(@PathVariable Long id) {
+        log.info("Request received find anime by id '{}'", id);
         return Anime.getAnimes()
                 .stream()
                 .filter(anime -> anime.getId().equals(id))
--- src/main/java/academy/devdojo/animeservice/domain/Anime.java
+++ src/main/java/academy/devdojo/animeservice/domain/Anime.java
@@ -1,29 +1,22 @@
 package academy.devdojo.animeservice.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.List;
 
+@Getter
+@AllArgsConstructor
 public class Anime {
 
     private Long id;
     private String name;
 
-    public Anime(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
     public static List<Anime> getAnimes() {
         var jigokuraku = new Anime(1L, "Jigokuraku");
-            var konosuba = new Anime(2L, "Konosuba");
+        var konosuba = new Anime(2L, "Konosuba");
         var drStone = new Anime(3L, "Dr.Stone");
         return List.of(jigokuraku, konosuba, drStone);
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
     }
 }
