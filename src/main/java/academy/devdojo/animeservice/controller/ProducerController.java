790cdb88c47601a7d4e67ce07051af027a4fe349
VictorDMoura <mouramvictor@gmail.com>
2024-07-02 19:56:43 -0300

    video17: Mapeando entidades com MapStruct

--- src/main/java/academy/devdojo/animeservice/controller/ProducerController.java
+++ src/main/java/academy/devdojo/animeservice/controller/ProducerController.java
@@ -1,6 +1,7 @@
 package academy.devdojo.animeservice.controller;
 
 import academy.devdojo.animeservice.domain.Producer;
+import academy.devdojo.animeservice.mapper.ProducerMapper;
 import academy.devdojo.animeservice.request.ProducerPostRequest;
 import academy.devdojo.animeservice.response.ProducerPostResponse;
 import org.springframework.http.HttpStatus;
@@ -11,9 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.concurrent.ThreadLocalRandom;
-
 @RestController
 @RequestMapping({"v1/producers", "v1/producers/"})
 public class ProducerController {
@@ -21,16 +19,10 @@ public class ProducerController {
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE,
             headers = "x-api-version=v1")
     public ResponseEntity<ProducerPostResponse> save(@RequestBody ProducerPostRequest request) {
-        var producer = Producer.builder()
-                .name(request.getName())
-                .id(ThreadLocalRandom.current().nextLong(100_000))
-                .createdAt(LocalDateTime.now())
-                .build();
+        var mapper = ProducerMapper.INSTANCE;
+        var producer = mapper.toProducer(request);
+        var response = mapper.toProducerPostResponse(producer);
         Producer.getProducers().add(producer);
-        var response = ProducerPostResponse.builder()
-                .name(producer.getName())
-                .id(producer.getId())
-                .build();
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
---
+++ src/main/java/academy/devdojo/animeservice/mapper/ProducerMapper.java
@@ -0,0 +1,20 @@
+package academy.devdojo.animeservice.mapper;
+
+import academy.devdojo.animeservice.domain.Producer;
+import academy.devdojo.animeservice.request.ProducerPostRequest;
+import academy.devdojo.animeservice.response.ProducerPostResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface ProducerMapper {
+
+    ProducerMapper INSTANCE = Mappers.getMapper(ProducerMapper.class);
+
+    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
+    @Mapping(target = "id", expression = "java(java.util.concurrent.ThreadLocalRandom.current().nextLong(100_000))")
+    Producer toProducer(ProducerPostRequest request);
+
+    ProducerPostResponse toProducerPostResponse(Producer producer);
+}
