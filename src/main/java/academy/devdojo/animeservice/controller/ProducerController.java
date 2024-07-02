62f07ddfecd9c7cf2e5fea1534e5bb311b718b66
VictorDMoura <mouramvictor@gmail.com>
2024-07-02 11:09:53 -0300

    video16: Criando objetos diferentes para requisição e resposta

--- src/main/java/academy/devdojo/animeservice/controller/ProducerController.java
+++ src/main/java/academy/devdojo/animeservice/controller/ProducerController.java
@@ -1,6 +1,8 @@
 package academy.devdojo.animeservice.controller;
 
 import academy.devdojo.animeservice.domain.Producer;
+import academy.devdojo.animeservice.request.ProducerPostRequest;
+import academy.devdojo.animeservice.response.ProducerPostResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
@@ -17,9 +20,18 @@ public class ProducerController {
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE,
             headers = "x-api-version=v1")
-    public ResponseEntity<Producer> save(@RequestBody Producer producer) {
-        producer.setId(ThreadLocalRandom.current().nextLong());
+    public ResponseEntity<ProducerPostResponse> save(@RequestBody ProducerPostRequest request) {
+        var producer = Producer.builder()
+                .name(request.getName())
+                .id(ThreadLocalRandom.current().nextLong(100_000))
+                .createdAt(LocalDateTime.now())
+                .build();
         Producer.getProducers().add(producer);
-        return ResponseEntity.status(HttpStatus.CREATED).body(producer);
+        var response = ProducerPostResponse.builder()
+                .name(producer.getName())
+                .id(producer.getId())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
