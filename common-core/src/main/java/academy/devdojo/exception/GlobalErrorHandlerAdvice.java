368451d3785e577ac76adedf9def9a3a350685f3
VictorDMoura <mouramvictor@gmail.com>
2024-10-02 20:36:40 -0300

    video:77 Handling SQLIntegrityConstraintViolationException

--- common-core/src/main/java/academy/devdojo/exception/GlobalErrorHandlerAdvice.java
+++ common-core/src/main/java/academy/devdojo/exception/GlobalErrorHandlerAdvice.java
@@ -5,13 +5,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 @ControllerAdvice
 public class GlobalErrorHandlerAdvice {
 
-    @ExceptionHandler
+    @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<DefaultErrorMessage> handlerNotFoundException(NotFoundException ex) {
         var errorResponse = new DefaultErrorMessage(HttpStatus.NOT_FOUND.value(), ex.getReason());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<DefaultErrorMessage> handlerSQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException ex) {
+        var errorResponse = new DefaultErrorMessage(HttpStatus.BAD_REQUEST.value(), "Integrity exception, one of the fields should be unique");
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
