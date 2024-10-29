cfdf6cb3564ef0fcf77154b91b3fb87f10cd9032
VictorDMoura <mouramvictor@gmail.com>
2024-10-29 11:17:30 -0300

    video:93 jpql

--- user-service/src/main/java/academy/devdojo/repository/UserProfileRepository.java
+++ user-service/src/main/java/academy/devdojo/repository/UserProfileRepository.java
@@ -2,10 +2,15 @@ package academy.devdojo.repository;
 
 import academy.devdojo.domain.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
 
+    @Query("SELECT up FROM UserProfile up join fetch up.user u join fetch up.profile p")
+    List<UserProfile> retrieveAll();
 
 }
