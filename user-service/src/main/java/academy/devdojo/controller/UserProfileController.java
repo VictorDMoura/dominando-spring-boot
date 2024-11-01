cef329ece95b3c943d2cd3d1e652fe41f4be19d9
VictorDMoura <mouramvictor@gmail.com>
2024-11-01 11:58:07 -0300

    Video 98: exercise user profile 04 - Mapper

--- user-service/src/main/java/academy/devdojo/controller/UserProfileController.java
+++ user-service/src/main/java/academy/devdojo/controller/UserProfileController.java
@@ -1,8 +1,9 @@
 package academy.devdojo.controller;
 
 
-import academy.devdojo.domain.User;
-import academy.devdojo.domain.UserProfile;
+import academy.devdojo.mapper.UserProfileMapper;
+import academy.devdojo.response.UserProfileGetResponse;
+import academy.devdojo.response.UserProfileUserGetResponse;
 import academy.devdojo.service.UserProfileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -21,24 +22,25 @@ import java.util.List;
 public class UserProfileController {
 
     private final UserProfileService service;
+    private final UserProfileMapper mapper;
 
     @GetMapping
-    public ResponseEntity<List<UserProfile>> list() {
+    public ResponseEntity<List<UserProfileGetResponse>> list() {
         log.debug("Request received to list all user profiles");
 
         var userProfiles = service.findAll();
+        var userProfileGetResponses = mapper.toUserProfileGetResponse(userProfiles);
 
-
-        return ResponseEntity.ok(userProfiles);
+        return ResponseEntity.ok(userProfileGetResponses);
     }
 
     @GetMapping("profiles/{id}/users")
-    public ResponseEntity<List<User>> listAllUsersByProfileId(@PathVariable Long id) {
+    public ResponseEntity<List<UserProfileUserGetResponse>> listAllUsersByProfileId(@PathVariable Long id) {
         log.debug("Request received to list all users by profile id {}", id);
 
-        var userProfiles = service.findAllUserByProfileId(id);
+        var users = service.findAllUserByProfileId(id);
+        var userProfileUserGetResponses = mapper.toUserProfileUserGetResponse(users);
 
-
-        return ResponseEntity.ok(userProfiles);
+        return ResponseEntity.ok(userProfileUserGetResponses);
     }
 }
